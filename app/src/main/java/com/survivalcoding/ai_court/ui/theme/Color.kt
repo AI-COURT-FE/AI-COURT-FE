589b00332cca944f404a00aefd43f886fa3cf1fd
kyungmin <minii7582@konkuk.ac.kr>
2026-01-06 17:52:27 +0900

    feat: theme수정

--- app/src/main/java/com/survivalcoding/ai_court/ui/theme/Color.kt
+++ app/src/main/java/com/survivalcoding/ai_court/ui/theme/Color.kt
@@ -2,10 +2,11 @@ package com.survivalcoding.ai_court.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Color.kt
+val Brown100 = Color(0xFFF6B165)
+val Brown200 = Color(0xFFD58D57)
+val Brown300 = Color(0xFFB36C49)
+val Brown400 = Color(0xFF914F3B)
+val Brown500 = Color(0xFF6F362D)
+val Brown600 = Color(0xFF4E211F)
+val Brown700 = Color(0xFF2C1112)
