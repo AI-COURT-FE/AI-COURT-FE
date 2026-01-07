f8055e240e9d2a886274590c1784c4a152bb441c
AyoungSe0 <ayoungseo1005@gmail.com>
2026-01-08 01:31:49 +0900

    불필요한 임포트 삭제

--- app/src/main/java/com/survivalcoding/ai_court/presentation/entry/screen/EntryScreen.kt
+++ app/src/main/java/com/survivalcoding/ai_court/presentation/entry/screen/EntryScreen.kt
@@ -1,9 +1,7 @@
 package com.survivalcoding.ai_court.presentation.entry.screen
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,18 +10,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -31,8 +25,6 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.survivalcoding.ai_court.core.component.CourtButton
 import com.survivalcoding.ai_court.presentation.entry.component.LogoSection
 import com.survivalcoding.ai_court.presentation.entry.component.NicknameInput
-import com.survivalcoding.ai_court.presentation.entry.component.RoomCodeInput
-import com.survivalcoding.ai_court.presentation.entry.component.WaitingDialog
 import com.survivalcoding.ai_court.presentation.entry.viewmodel.EntryViewModel
 import com.survivalcoding.ai_court.ui.theme.AI_COURTTheme
 
