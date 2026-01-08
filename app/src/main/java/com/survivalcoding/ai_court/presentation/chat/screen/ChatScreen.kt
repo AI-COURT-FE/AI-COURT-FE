00f95c4fb3dac862c73843034454a27fa3c5f6a2
AyoungSe0 <ayoungseo1005@gmail.com>
2026-01-09 08:07:48 +0900

    fix: 뒤로가기 모두 삭제 입장코드로 입장할 때 인디케이터 위치 수정

--- app/src/main/java/com/survivalcoding/ai_court/presentation/chat/screen/ChatScreen.kt
+++ app/src/main/java/com/survivalcoding/ai_court/presentation/chat/screen/ChatScreen.kt
@@ -16,13 +16,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.ime
-import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBars
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.union
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
